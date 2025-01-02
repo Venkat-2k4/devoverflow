a9cebc234e7f665af612d36c8bfd9d8bc5e32335
venkat <venkatmanikantakarri@gmail.com>
2025-01-02 10:48:14 +0530

    configuring tailwind styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl text-center ">
+      <h1 className="text-3xl text-light-500 text-center ">
         this is an new next js application
       </h1>
     </div>
