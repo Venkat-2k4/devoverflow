e1b1182d518e761b4a6242edf2c1612ad1b94877
venkat <venkatmanikantakarri@gmail.com>
2025-01-03 18:33:40 +0530

    tailwind css setup and theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl text-light-500 text-center ">
+      <h1 className="background-light850_dark100 text-dark-100  h1-bold">
         this is an new next js application
       </h1>
     </div>
