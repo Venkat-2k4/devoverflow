077ece7c1fc86d859841c8b8dac591a67822675d
venkat <venkatmanikantakarri@gmail.com>
2025-01-03 19:40:48 +0530

    configured fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="background-light850_dark100 text-dark-100  h1-bold">
-        this is an new next js application
+      <h1 className=" h1-bold">this is an new next js application</h1>
+      <h1 className="h1-bold font-space-grotesk">
+        {" "}
+        this is a new next js application{" "}
       </h1>
     </div>
   );
