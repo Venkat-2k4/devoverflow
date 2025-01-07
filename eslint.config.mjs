0bf73bd30c42a30111d606fc411b4dc39483da8b
venkat <venkatmanikantakarri@gmail.com>
2025-01-07 16:58:54 +0530

    eslint updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,17 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-const eslintConfig = [
+export default [
   ...compat.extends(
     "next/core-web-vitals",
     "next/typescript",
@@ -17,47 +19,9 @@ const eslintConfig = [
     "plugin:tailwindcss/recommended",
     "prettier"
   ),
-
   {
-    plugins: ["import"],
     rules: {
-      "import/order": [
-        "error",
-        {
-          groups: [
-            "builtin", // Built-in types are first
-            "external", // External libraries
-            "internal", // Internal modules
-            ["parent", "sibling"], // Parent and sibling types can be mingled together
-            "index", // Then the index file
-            "object", // Object imports
-          ],
-          "newlines-between": "always",
-          pathGroups: [
-            {
-              pattern: "@app/**",
-              group: "external",
-              position: "after",
-            },
-          ],
-          pathGroupsExcludedImportTypes: ["builtin"],
-          alphabetize: {
-            order: "asc",
-            caseInsensitive: true,
-          },
-        },
-      ],
+      "no-undef": "off",
     },
-    ignorePatterns: ["components/ui/**"],
-    overrides: [
-      {
-        files: ["*.ts", "*.tsx"],
-        rules: {
-          "no-undef": "off",
-        },
-      },
-    ],
   },
 ];
-
-export default eslintConfig;
